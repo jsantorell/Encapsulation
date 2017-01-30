b1c4519416891e313b4149f14486492a399f5367
jerem <jerem@192.168.88.1>
2017-01-30 14:55:40 -0600

    encapsulated lab 1

--- src/lab1/Employee.java
+++ src/lab1/Employee.java
@@ -12,16 +12,114 @@ import java.util.Date;
  * @version     1.02
  */
 public class Employee {
-    String firstName;
-    String lastName;
-    public String ssn;
-    boolean metWithHr;
-    boolean metDeptStaff;
-    boolean reviewedDeptPolicies;
+    private String firstName;
+    private String lastName;
+    private String ssn;
+    private boolean metWithHr;
+    private boolean metDeptStaff;
+    private boolean reviewedDeptPolicies;
     boolean movedIn;
-    String cubeId;
-    Date orientationDate;
+    private String cubeId;
+    private Date orientationDate;
 
+    public Employee(String firstName, String lastName, String ssn, boolean metWithHr, boolean metDeptStaff, boolean reviewedDeptPolicies, boolean movedIn, String cubeId, Date orientationDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ssn = ssn;
+        this.metWithHr = metWithHr;
+        this.metDeptStaff = metDeptStaff;
+        this.reviewedDeptPolicies = reviewedDeptPolicies;
+        this.movedIn = movedIn;
+        this.cubeId = cubeId;
+        this.orientationDate = orientationDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        
+        if(ssn == null || ssn.length() <9 || ssn.length() >9){
+        
+            System.out.println("Please provide your full Social Security Number without spaces.");
+            return;
+        }
+        
+        
+        this.ssn = ssn;
+    }
+
+    public boolean isMetWithHr() {
+        return metWithHr;
+    }
+
+    public void setMetWithHr(boolean metWithHr) { // does not require valid
+        this.metWithHr = metWithHr;
+    }
+
+    public boolean isMetDeptStaff() {
+        return metDeptStaff;
+    }
+
+    public void setMetDeptStaff(boolean metDeptStaff) { // does not require valid
+        this.metDeptStaff = metDeptStaff;
+    }
+
+    public boolean isReviewedDeptPolicies() {
+        return reviewedDeptPolicies;
+    }
+
+    public void setReviewedDeptPolicies(boolean reviewedDeptPolicies) { // does not require valid
+        this.reviewedDeptPolicies = reviewedDeptPolicies;
+    }
+
+    public boolean isMovedIn() {
+        return movedIn;
+    }
+
+    public void setMovedIn(boolean movedIn) { // does not require valid
+        this.movedIn = movedIn;
+    }
+
+    public String getCubeId() {
+        return cubeId;
+    }
+
+    public void setCubeId(String cubeId) {
+        this.cubeId = cubeId;
+    }
+
+    public Date getOrientationDate() {
+        return orientationDate;
+    }
+
+    public void setOrientationDate(Date orientationDate) {
+        
+        if(orientationDate == null){
+        
+            
+        }
+        
+        this.orientationDate = orientationDate;
+    }
+    
 
 
 }
